f9a23e480f566f07b941b04dad74843e0c11695c
Sofiane KHATIR <skhatir@e2r11p12.42.fr>
2016-10-22 04:43:47 +0200

    fix_bugs_an add_color

--- sniff_back.py
+++ sniff_back.py
@@ -1,19 +1,26 @@
 import urllib2
- 
-list=[".backup",".bck",".old",".save",".bak",".sav","~",".copy",".old",".orig",".tmp",".txt",".back",".bkp",".bac",".tar",".gz",".tar.gz",".zip",".rar"]
- 
-hote = sys.argv[1] #url
+import sys
 
-fichier = sys.argv[2] #file name ec: index
 
-ext = sys.argv[3] #.php .html ...
- 
-for item in list:
- 
-   try:
-        url = hote + "" + fichier + "" + ext + item
-        result = urllib2.urlopen(url)
-        print url + " Code : "+str(result.getcode())
- 
-   except urllib2.HTTPError as e:
-            print url+" Code :  " + str(e)
+def main():
+	"""
+	"""
+	list_ = [".backup",".bck",".old",".save",".bak",".sav","~",".copy",".old",".orig",".tmp",".txt",".back",".bkp",".bac",".tar",".gz",".tar.gz",".zip",".rar", ".ini", ".ini.php", ".php"]
+	hote = sys.argv[1] #url
+
+	fichier = sys.argv[2] #file name ec: index
+
+	ext = sys.argv[3] #.php .html ...
+	 
+	for item in list_:
+	 
+	   try:
+	        url = hote + "" + fichier + "" + ext + item
+	        result = urllib2.urlopen(url)
+	        print "\033[32m" + url + " Code : "+str(result.getcode()) + "\033[39m"
+	 
+	   except urllib2.HTTPError as e:
+	            print "\033[31m" + url+" Code :  " + str(e) + "\033[39m"
+
+if __name__ == '__main__':
+	main()
