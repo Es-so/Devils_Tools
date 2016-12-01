fc61cc1f80cae04784cf9686b4bb7d5e5bbe8f01
Sofiane KHATIR <skhatir@e3r6p20.42.fr>
2016-12-01 01:57:15 +0100

    messageSender_

--- test_AUTO_FB_LOGGER_and_SEND_MESS/connect2fb.py
+++ test_AUTO_FB_LOGGER_and_SEND_MESS/connect2fb.py
@@ -8,94 +8,127 @@ import fbchat
 import requests
 from lxml import html
 
+
+#__________DATA_REQUIEREMENT_____________________________________________________#
+
 USERNAME = raw_input('LOGIN/MAIL: '); 
 PASSWORD = raw_input('MDP: ');
-
 LOGIN_URL = "https://www.facebook.com"
 TARGET_MP = raw_input('FRIENDS: ');
+TARGET = TARGET_MP;
 TARGET_MP.replace(' ', '.');
 # URL_MESS = "https://www.facebook.com/messages/" + TARGET_MP;
 URL_MESS = "https://www.facebook.com/messages/victor.danain"
 
+# __MULTIPLE_MESSAGES_USER_______________________________
+
+with open(raw_input("TARGETS USERS FILE: "), "r") as ins:
+    TARGETS = []
+    for line in ins:
+        TARGETS.append(line)
+
+#_______________________________________________________
+
+
+#__________END_DATA_REQUIEREMENT_________________________________________________#
+
+
+
 br = Browser()
-# br.set_all_readonly(False) 
-
-# Ignore robots.txt
 br.set_handle_robots( False )
-
-# Google demands a useragent that isn't a robot
 br.addheaders = [('User-agent', 'Firefox')]
-
 br.open(LOGIN_URL)
 
-br.select_form(nr=0)
-tophp = br.response().read()
-print tophp
+# print  br.response().read()
 
-br['email'] = USERNAME
-br['pass'] = PASSWORD
+# br.select_form(nr=0)
 
-response = br.submit()
+# br['email'] = USERNAME
+# br['pass'] = PASSWORD
 
-reponse_page = response.read()
-
-print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
-________________________________________________________________________________\
-\	\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-
-print reponse_page
-
-page_mess = br.open(URL_MESS)
-
-for form in br.forms():
-    print "[" + str(form) + "]"
-
-br.select_form(nr=0)
-br['email'] = USERNAME
-br['pass'] = PASSWORD
-
-for form in br.forms():
-    print "[" + str(form) + "]"
+# response = br.submit()
 
 
-response = br.submit()
+# print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
+# ________________________________________________________________________________\
+# \	\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+
+# reponse_page = response.read()
+# print reponse_page
+
+# page_mess = br.open(URL_MESS)
+
+# for form in br.forms():
+#     print "[" + str(form) + "]"
+
+# br.select_form(nr=0)
+# br['email'] = USERNAME
+# br['pass'] = PASSWORD
+
+# for form in br.forms():
+#     print "[" + str(form) + "]"
 
 
-print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
-________________________________________________________________________________\
-\	\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-
-dd = response.read()
-ff = br.response()
-
-print ">>>>" + dd + "<<"
-
-print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
-________________________________________________________________________________\
-\	\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+# response = br.submit()
 
 
-# br.forms().set_all_readonly(False)
+# print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
+# ________________________________________________________________________________\
+# \	\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 
-# form.set_value("rhubarb rhubarb", kind="text", nr=0)
-# form.find_control("foo").readonly = False
-# form.set_all_readonly(False)
+# dd = response.read()
+# ff = br.response()
 
-form.new_control('text','message_body',{'value':''})
-form.fixup()
-form['message_body'] = 'input'
+# print ">>>>" + dd + "<<"
 
-for form in br.forms():
-    print "[" + str(form) + "]"
+# print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
+# ________________________________________________________________________________\
+# \	\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+
+
+# form.new_control('text','message_body',{'value':''})
+# form.fixup()
+# form['message_body'] = 'input'
+
+# for form in br.forms():
+#     print "[" + str(form) + "]"
 
 client = fbchat.Client(USERNAME, PASSWORD)
-friend = client.getUsers("victor danain")
+friend = client.getUsers(TARGET)
+
+
+friends = []
+for selected_target in TARGETS:
+	print selected_target + "<-"*47
+	friends.append(client.getUsers(selected_target))
+
+	
+
+
+friend_info = client.getUserInfo(friend[0].uid)
+
+for f in friends:
+	print ">"*50 + "\n" + str(f[0])
+	client.send(f[0].uid, "message personnalise pour mon petit: " + str(f[0].name))
+
+
+# print "|"*80 + str(friends[1]) + "|"*80
 
 # for friend in friends: 
-print friend[0]
-sent = client.send(friend[0].uid, "Your Message")
-if sent:
-    print("Message sent successfully!")
+
+# sent = client.send(friend[0].uid, raw_input("MESSAGE: "))
+# if sent:
+#     print("Message sent successfully!")
+
+print friend_info
+
+last_messages = client.getThreadInfo(friend[0].uid,0)
+last_messages.reverse()  # messages come in reversed order
+
+for message in last_messages:
+    print(message.body)
+
+print (last_messages)
 
 
 # import re
---
+++ test_AUTO_FB_LOGGER_and_SEND_MESS/messageSender.fb.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+from mechanize import Browser
+import os
+import sys
+import fbchat
+import requests
+from lxml import html
+from subprocess import call
+
+os.system("clear")
+
+#__________DATA_REQUIEREMENT_____________________________________________________#
+
+USERNAME = raw_input('LOGIN/MAIL: '); 
+PASSWORD = raw_input('MDP: ');
+LOGIN_URL = "https://www.facebook.com"
+TARGET_MP = raw_input('FRIENDS: ');
+TARGET = TARGET_MP;
+URL_MESS = "https://www.facebook.com/messages/" + TARGET_MP.replace(' ', '.');
+
+
+# __MULTIPLE_MESSAGES_USER_______________________________
+
+
+try:
+	with open(raw_input("TARGETS USERS FILE: "), "r") as ins:
+	    TARGETS = []
+	    for line in ins:
+	        TARGETS.append(line)
+	friend = None
+
+except:
+	TARGETS = TARGET
+	if TARGETS == None :
+		print "No TARGET(S)"
+		sys.exit()
+
+#_______________________________________________________
+
+MESSAGE = raw_input('MESSAGE: ')
+
+print ("\n" + "_" * 60 + "\n\033[33m Resume:\033[39m\n" +
+	"USERNAME: " + USERNAME + "\n" +
+	"URL: " + LOGIN_URL + "\n" +
+	"MESSAGE: " + MESSAGE + "\n" +
+	"TARGETS: " + str(TARGETS) + "\n" + "_" * 60);
+
+#__________END_DATA_REQUIEREMENT_________________________________________________#
+
+client = fbchat.Client(USERNAME, PASSWORD);
+
+print "\n\n\033[33mSending...\033[39m\n"
+
+if friend is None:
+	friends = []
+	for selected_target in TARGETS:
+		friends.append(client.getUsers(selected_target))
+	try:
+		for f in friends:
+			save_stdout = sys.stdout
+			sys.stdout = open('/dev/null', 'w')
+			sent = client.send(f[0].uid, "Wesh " + str(f[0].name) + " " + MESSAGE);
+			sys.stdout = save_stdout
+			if sent:
+				print (str(f[0].name) + "\033[32m [√] \033[39m")
+			else:
+				print (str(f[0].name) + "\033[31m [X] \033[39m")
+	except:
+		print ("\033[31m Error \033[39m")
+
+else:
+	friend = client.getUsers(TARGET)
+	save_stdout = sys.stdout
+	sys.stdout = open('/dev/null', 'w')
+	sent = client.send(friend[0].uid, "Wesh " + str(friend[0].name) + " " + MESSAGE);
+	sys.stdout = save_stdout
+	if sent:
+		print (str(friend[0].name) + "\033[32m [√] \033[39m")
+	else:
+		print (str(friend[0].name) + "\033[31m [X] \033[39m")
+
+
+print ("\n\033[32m ... End message\033[39m\n")
